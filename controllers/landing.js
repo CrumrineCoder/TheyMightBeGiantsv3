8f32e1e5f9568d5141bc47cf028dfe9786aa6649
CrumrineCoder <crumrinecoding@gmail.com>
2018-08-17 22:44:41 -0400

    Touring See Nearest Tour Fixed!

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const apiUrl = 'http://localhost:5000/';
+const apiUrl = 'https://tmbg.herokuapp.com/';
 
 
 
@@ -79,11 +79,11 @@ function Deg2Rad(deg) {
 
         function showTour(data) {
             var TourObject = JSON.parse(data);
-            var dates = TourObject[0].dates;
+            var dates = TourObject[0].dates; 
             // Show the Comments
             for (var i = 0; i < dates.length; i++) {
                // ajaxRequest('GET', apiUrl + "geocoding/?address=" + dates[i].location + "&id=" + dates[i]._id, showGeo);
-               ajaxRequest('GET', "https://nominatim.openstreetmap.org/search/" + dates[i].location + "?format=json&addressdetails=1&limit=1&polygon_svg=1", showGeo)
+               ajaxRequest('GET', "https://nominatim.openstreetmap.org/search/" + dates[i].location + "?format=json&addressdetails=1&limit=1&polygon_svg=1", showGeo, dates[i]["_id"])
                 $scope.$apply(function () {
                     $scope.Tours.push(dates[i]);
                 });
@@ -92,18 +92,16 @@ function Deg2Rad(deg) {
         }
 
         $scope.Coords = [];
-        $scope.id = 0; 
 
-        function showGeo(data) {
+        function showGeo(data, id) {
             var object = JSON.parse(data);
             var lat = object[0].lat; 
             var lng = object[0].lon; 
             var address = object[0].display_name; 
             $scope.$apply(function () {
-                $scope.Coords.push({ "lat": lat, "lng": lng, "address": address });
+                $scope.Coords.push({ "lat": lat, "lng": lng, "address": address, "id": id });
                 $scope.id++; 
             });
-            console.log($scope.Coords);
             
       /*      var object = (JSON.parse(JSON.parse(data).geoData.body));
             var id = JSON.parse(data).id;
@@ -135,8 +133,6 @@ function Deg2Rad(deg) {
                     mindif = dif;
                 }
             }
-            console.log($scope.Tours);
-            console.log($scope.Coords); 
             for (var i = 0; i < $scope.Tours.length; i++) {
                 if ($scope.Tours[i]["_id"] === $scope.Coords[closest].id) {
                     $scope.$apply(function () {
